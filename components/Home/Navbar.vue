08c03db309c4c35719cf59563be0be5bebad5cdf
jaykobpc <watenga.jacob@gmail.com>
2022-07-19 17:05:10 +0300

    navbar updates

--- components/Home/Navbar.vue
+++ components/Home/Navbar.vue
@@ -1,46 +1,59 @@
 <template>
-  <nav class="navbar">
+  <nav class="navbar no-select">
     <div class="navbar__wrapper">
+      <!-- mobile -->
       <div class="navbar__toplayer">
         <div role="button" class="navbar__locationbtn">
           <div class="navbar__locationbtn__logo"></div>
           <div class="navbar__locationbtn__textbox">
-            <span class="navbar__locationbtn__textbox__smtitle">Delivery . Now</span>
+            <span class="navbar__locationbtn__textbox__smtitle">Delivery · Now</span>
             <h3 class="navbar__locationbtn__textbox__title">
               Selected location
             </h3>
           </div>
-          <Swiper :options="swiperOptions">
-            <SwiperSlide>
-              <button>Hello world!</button>
-            </SwiperSlide>
-          </Swiper>
+         <span class="navbar__locationbtn__icon">
+           <i class='bx bx-chevron-down'></i>
+         </span>
+        </div>
+        <div class="navbar__profilebox">
+          <img src="@/static/Profiles/user_1.jpg" alt="Profile Image" class="navbar__profilebox__img">
         </div>
       </div>
+      <!-- desktop -->
       <div class="navbar__bottomlayer">
-        <span>bottom</span>
+        <div role="img" class="navbar__bottomlayer__brandlogo"></div>
+        <div class="navbar__bottomlayer__searchbox">
+         <div class="navbar__bottomlayer__inputbox">
+           <i class='navbar__bottomlayer__inputbox__icon bx bx-search' ></i>
+           <input type="text" placeholder="Restaurants, groceries, dishes" class="navbar__bottomlayer__inputbox__input">
+         </div>
+         <span class="navbar__bottomlayer__searchbox__icon">
+            <i class='bx bx-slider-alt'></i>
+         </span>
+        </div>
+        <div class="navbar__bottomlayer__widgets">
+          <div role="button" class="navbar__bottomlayer__navbtn">
+            <i class='bx bx-basket' ></i>
+            <span>£0.00</span>
+          </div>
+          <div role="button" class="navbar__bottomlayer__navbtn">
+           <i class='bx bx-menu' ></i>
+            <span>Menu</span>
+          </div>
+        </div>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+
 export default {
   name: "Navbar",
-  components: {
-    Swiper,
-    SwiperSlide,
-  },
   data() {
     return {
-      swiperOptions: {
-        slidesPerView: 5,
-        spaceBetween: 30,
-        freeMode: true,
-        loop: false,
-      },
+     
     };
   },
 };
-</script>u
+</script>
--- plugins/vue-awesome-swiper.js
+++ plugins/vue-awesome-swiper.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueAwesomeSwiper from "vue-awesome-swiper";
 
 // import style
-import 'swiper/dist/css/swiper.min.css';
+import 'swiper/css/swiper.min.css';
+import 'boxicons/css/boxicons.min.css'
 
 Vue.use(VueAwesomeSwiper);
