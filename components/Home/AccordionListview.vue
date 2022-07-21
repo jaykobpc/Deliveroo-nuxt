191744e4463fafadaff6ab9a99067a324813eee4
jaykobpc <watenga.jacob@gmail.com>
2022-07-21 22:13:49 +0300

    card content

--- components/Home/AccordionListview.vue
+++ components/Home/AccordionListview.vue
@@ -13,14 +13,14 @@
       <CheckBox title="Hygiene rating: 3" name="hygiene-rating" :checked="false" />
       <CheckBox title="Hygiene rating: 2" name="hygiene-rating" :checked="false" />
     </Accordion>
-    <Accordion title="Sort" :isOpen="false">
-      <RadioButton title="All Offers" name="sort-group" :checked="true" />
-      <RadioButton title="Offers near you" name="sort-group" :checked="false" />
-      <RadioButton title="Restaurant picks" name="sort-group" :checked="false" />
-      <RadioButton title="Order more, save more" name="sort-group" :checked="false" />
-      <RadioButton title="Flash deals" name="sort-group" :checked="false" />
-      <RadioButton title="Free Items" name="sort-group" :checked="false" />
-      <RadioButton title="Free Delivery" name="sort-group" :checked="false" />
+    <Accordion title="Offers" :isOpen="false">
+      <RadioButton title="All Offers" name="offers-group" :checked="true" />
+      <RadioButton title="Offers near you" name="offers-group" :checked="false" />
+      <RadioButton title="Restaurant picks" name="offers-group" :checked="false" />
+      <RadioButton title="Order more, save more" name="offers-group" :checked="false" />
+      <RadioButton title="Flash deals" name="offers-group" :checked="false" />
+      <RadioButton title="Free Items" name="offers-group" :checked="false" />
+      <RadioButton title="Free Delivery" name="offers-group" :checked="false" />
     </Accordion>
   </div>
 </template>
